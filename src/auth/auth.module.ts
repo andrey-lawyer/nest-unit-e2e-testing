a645a67c812ec8d874ce5a08e72d055e79036132
andrey <uvarov.advokat@gmail.com>
2024-04-22 09:48:56 +0300

    fixed auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,106 +1,31 @@
-import { Test } from '@nestjs/testing';
-import { AppModule } from '../src/app.module';
-import * as request from 'supertest';
-import { DataSource } from 'typeorm';
+import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './strategy/jwt.strategy';
 
-import { SignUpDto } from '../src/auth/dto/signup.dto';
-import { userStub } from './stubs/user.stab';
-import { DatabaseService } from '../src/database/database.service';
-import { User } from '../src/auth/user.entity';
-
-import { Post } from '../src/posts/post.entity';
-import { LoginDto } from 'src/auth/dto/login.dto';
-
-describe('UsersController', () => {
-  let appDataSource: DataSource;
-  let httpServer: any;
-  let app: any;
-
-  beforeAll(async () => {
-    const moduleRef = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
-
-    app = moduleRef.createNestApplication();
-    await app.init();
-
-    appDataSource = moduleRef
-      .get<DatabaseService>(DatabaseService)
-      .getDbHandle();
-
-    httpServer = app.getHttpServer();
-  });
-
-  afterAll(async () => {
-    await appDataSource.getRepository(Post).delete({});
-    await appDataSource.getRepository(User).delete({});
-    await app.close();
-  });
-
-  describe('createUser', () => {
-    it('should create a user', async () => {
-      const createUserRequest: SignUpDto = {
-        name: userStub().name,
-        email: userStub().email,
-        password: userStub().password,
-      };
-
-      const responseUserCreate = {
-        name: userStub().name,
-        email: userStub().email,
-      };
-      const response = await request(httpServer)
-        .post('/auth/signup')
-        .send(createUserRequest);
-
-      expect(response.status).toBe(201);
-      expect(response.body).toMatchObject(responseUserCreate);
-
-      const user = await appDataSource.getRepository(User).findOneBy({
-        email: createUserRequest.email,
-      });
-      expect(user).toMatchObject(responseUserCreate);
-    });
-  });
-
-  describe('loginUser', () => {
-    it('should login user', async () => {
-      const loginUserRequest: LoginDto = {
-        email: userStub().email,
-        password: userStub().password,
-      };
-
-      const response = await request(httpServer)
-        .post('/auth/login')
-        .send(loginUserRequest);
-      expect(response.status).toBe(200);
-    });
-
-    it('should return unauthorized', async () => {
-      const loginUserRequest: LoginDto = {
-        email: 'new' + userStub().email,
-        password: userStub().password,
-      };
-
-      const response = await request(httpServer)
-        .post('/auth/login')
-        .send(loginUserRequest);
-      expect(response.status).toBe(401);
-    });
-  });
-
-  describe('getUsers', () => {
-    const users = [
-      {
-        name: userStub().name,
-        email: userStub().email,
+@Module({
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          secret: config.get<string>('JWT_SECRET'),
+          signOptions: {
+            expiresIn: config.get<string | number>('JWT_EXPIRES'),
+          },
+        };
       },
-    ];
-    it('should return users', async () => {
-      const response = await request(httpServer).get('/auth/users');
-      expect(response.status).toBe(200);
-
-      expect(response.body).toMatchObject(users);
-    });
-  });
-});
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, JwtStrategy],
+  exports: [JwtStrategy, PassportModule],
+})
+export class AuthModule {}
